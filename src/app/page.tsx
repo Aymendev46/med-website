e8a47223091ea913bc82a369c57615751f881c56
Aymendz46 <64426918+Aymendz46@users.noreply.github.com>
2025-09-18 19:10:24 +0100

    Updated logo size to 46

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
               width={58}
               height={58}
               priority
-              className="h-14 w-14"
+              className="h-14 w-58"
             />
             <div>
             </div>
